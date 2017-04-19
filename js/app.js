9bfb8c7bbb23a6c280fec1e5591a5b3f352c6570
jmt75200 <joelle@pcgamerzhawaii.com>
2017-04-19 08:28:56 -1000

    adding show comments

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@
 /* Show Overlay
 When overlay button is clicked add style visiblility: visible to overlay-container
 */
+  console.log('hello');
 var lrgButtonEl = document.getElementById('lrg-overlay');
 var smlButtonEl = document.getElementById('sml-overlay');
 var lrgOverlay = document.getElementById('large');
 var smlOverlay = document.getElementById('small');
 
 lrgButtonEl.addEventListener('click', function() {
-  console.log('hello');
   lrgOverlay.style.visibility = 'visible';
 });
 
@@ -38,7 +38,25 @@ When view comments is click add style visibility: visible or add class open to t
 Remove Class Hidden
 */
 
+var formEl = document.getElementsByClassName('form');
+function removeClass(el) {
+  console.log('removeclass',formEl[0].classList);
+  console.log('el',el.nextElementSibling);
+  el.nextElementSibling.classList.remove('hidden');
+}
+
+var commentEl = document.getElementsByClassName('comment');
+console.log('commentEl',commentEl.length);
+
+for (var i = 0; i < commentEl.length; i++){
+  console.log('commentEl[i]',commentEl[i]);
+  console.log('i',i);
+  commentEl[i].addEventListener('click', function() {
+    removeClass(this);
+  });
+}
+
+
 /* Hide Comments
-When view comments is visible add style visibility: hidden or add class hidden to the form element
-Remove Class Open
+When view comments is visible add style visibility: hidden or add class hidden to the form element Remove Class Open
 */
